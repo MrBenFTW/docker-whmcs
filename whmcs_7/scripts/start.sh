c2503c4aed2a32f2923065f013c3163a95fb38c1
root <root@tools01.ams3.dots.dk>
2018-08-03 09:08:54 +0000

    Added cron and bumped version to 7.6.0

--- whmcs_7/scripts/start.sh
+++ whmcs_7/scripts/start.sh
@@ -21,64 +21,31 @@ if [ ! -e /.first-run-complete ]; then
   
   cp /tmp/ioncube/ioncube_loader_lin_5.6.so /opt/remi/php56/root/usr/lib64/php/modules
   chmod 755 /opt/remi/php56/root/usr/lib64/php/modules/ioncube_loader_lin_5.6.so
-  PHPVERSION=$(php-fpm --version | grep '^PHP' | sed 's/PHP \([0-9]\.[0-9]*\).*$/\1/')
+
   #extract whmcs into place
   rm -f /usr/share/nginx/html/*.html
-  unzip -q /whmcs.zip -d  /usr/share/nginx/html && mv /usr/share/nginx/html/whmcs /usr/share/nginx/html/members
+  #unzip -q /whmcs.zip -d  /usr/share/nginx/html && mv /usr/share/nginx/html/whmcs /usr/share/nginx/html/
+  unzip /whmcs.zip -d /usr/share/nginx/html && mv /usr/share/nginx/html/whmcs/* /usr/share/nginx/html && rmdir /usr/share/nginx/html/whmcs
+  touch /usr/share/nginx/html/configuration.php && chown nginx:nginx /usr/share/nginx/html/configuration.php && chmod 0777 /usr/share/nginx/html/configuration.php
   rm -f /whmcs.zip
 
   #extract xero-addon into place
-  unzip -q /xero.zip -d  /usr/share/nginx/html/members
+  unzip -q /xero.zip -d  /usr/share/nginx/html/
   rm -f /xero.zip
-
-  #Remove install
-  #rm -rf /usr/share/nginx/html/members/install directory
-
-  #sort permissions on configuration.php file
-  touch /usr/share/nginx/html/members/configuration.php
-  chown nginx:nginx /usr/share/nginx/html/members/configuration.php && chmod 0777 /usr/share/nginx/html/members/configuration.php
 
   echo "Do not remove this file." > /.first-run-complete
 fi
 
+if [ -s /usr/share/nginx/html/configuration.php ]
+then
+  echo "File has something, deleting install dir";
+  rm -rf /usr/share/nginx/html/install/
+else
+  echo "File is empty, not deleting install dir";
+fi
+
 # Again set the right permissions (needed when mounting from a volume)
-chown -Rf nginx:nginx /usr/share/nginx/html/members/
+chown -Rf nginx:nginx /usr/share/nginx/html/
 
 # Start the first process
 /usr/sbin/php-fpm -c /etc/php-fpm.conf && /usr/sbin/nginx
-
-
-#status=$?
-#if [ $status -ne 0 ]; then
-#  echo "Failed to start php-fpm: $status"
-#  exit $status
-#fi
-
-# Start the second process
-#/usr/sbin/nginx
-#status=$?
-#if [ $status -ne 0 ]; then
-#  echo "Failed to start nginx: $status"
-#  exit $status
-#fi
-
-# Naive check runs checks once a minute to see if either of the processes exited.
-# This illustrates part of the heavy lifting you need to do if you want to run
-# more than one service in a container. The container exits with an error
-# if it detects that either of the processes has exited.
-# Otherwise it loops forever, waking up every 60 seconds
-
-#while sleep 60; do
-#  ps aux |grep php-fpm |grep -q -v grep
-#  PROCESS_1_STATUS=$?
-#  ps aux |grep nginx |grep -q -v grep
-#  PROCESS_2_STATUS=$?
-  # If the greps above find anything, they exit with 0 status
-  # If they are not both 0, then something is wrong
-#  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
-#    echo "One of the processes has already exited."
-#    exit 1
-#  fi
-#done
-
-
